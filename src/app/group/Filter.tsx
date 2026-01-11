51d47b367d484a6a5398d16405a33960072106d5
yama.can <yama.can95@gmail.com>
2026-01-11 14:52:55 +0900

    [add] Add groups page

---
+++ src/app/group/Filter.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Selector, { SelectorGroup } from "@/components/selector";
+import { useState } from "react";
+
+enum MethodCategory {
+	OFFLINE = '会場',
+	MAGAZINE = '部誌・団体誌',
+};
+
+enum GroupCategory {
+	CLUB = '部活動・同好会',
+	COMMITTEE = '学芸大会実行委員会',
+	VOLUNTEER = '有志団体',
+};
+
+export function GroupFilter() {
+
+	const [methodCategory, setMethodCategory] = useState<number>(0);
+	const [groupCategory, setGroupCategory] = useState<number>(0);
+
+	return (
+		<div
+			style={{
+				width: "300px",
+			}}
+		>
+
+			<SelectorGroup>
+
+				<Selector
+					title="団体カテゴリ"
+					vertical
+					items={Object.values(GroupCategory)}
+					state={[groupCategory, setGroupCategory]}
+				/>
+
+				<Selector
+					title="参加方法"
+					vertical
+					items={Object.values(MethodCategory)}
+					state={[methodCategory, setMethodCategory]}
+					backgrounds={
+						[
+							"var(--theme-common-weak0-foreground)",
+							"var(--theme-common-weak1-foreground)"
+						]
+					}
+				/>
+
+			</SelectorGroup>
+
+		</div>
+	)
+
+}
---
+++ src/app/group/page.tsx
@@ -0,0 +1,13 @@
+import Heading from "@/components/heading";
+import { GroupFilter } from "./Filter";
+
+export default function GroupPage() {
+
+	return (
+		<main>
+			<Heading>団体一覧</Heading>
+			<GroupFilter />
+		</main>
+	)
+
+}
